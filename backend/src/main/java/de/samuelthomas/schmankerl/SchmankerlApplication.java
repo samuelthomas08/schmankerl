a4c4d1cfe68132ecc0c6aee3408d0142af94a04a
samuelthomas08 <thomas08samuel@gmail.com>
2026-08-22 11:31:24 +0200

    feat: Initial Commit

---
+++ backend/src/main/java/de/samuelthomas/schmankerl/SchmankerlApplication.java
@@ -0,0 +1,13 @@
+package de.samuelthomas.schmankerl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchmankerlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchmankerlApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/de/samuelthomas/schmankerl/SchmankerlApplicationTests.java
@@ -0,0 +1,13 @@
+package de.samuelthomas.schmankerl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchmankerlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
